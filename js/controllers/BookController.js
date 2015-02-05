cf3ecae9e3668bc3c07953314f75ab7aab7ad919
Lalith Polepeddi <lpolepeddi@gmail.com>
2015-02-05 18:45:44 -0500

    Update BookController.js

--- js/controllers/BookController.js
+++ js/controllers/BookController.js
@@ -1,4 +1,5 @@
 app.controller('BookController', ['$scope', '$routeParams', function($scope, $routeParams) {
+  // Your code here
   
 
   // Using this property to create the URL in line 9 of views/book.html
