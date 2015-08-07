e9f03d9acc436d85d8719eaed8e9ae991b85eded
jbreezy11 <jeryl.barnum@gmail.com>
2015-08-06 21:31:07 -0700

    added comments

--- js/controllers/BookController.js
+++ js/controllers/BookController.js
@@ -1,9 +1,14 @@
 app.controller('BookController', ['$scope', 'books', '$routeParams', function($scope, books, $routeParams) {
-  // Your code here
+  //uses the 'books' service to load a single book into $scope.book property.
+  // uses 'books' service to fetch an array of books from the server then uses 
+  // $routeParams.bookId to access teh spcific book by index and store it into $scope.book
+
   books.success(function(data) {
     $scope.book = data[$routeParams.bookId];
   });
+  // Remember we can use Angular's $routeParams to retrieve bookId from the URL by using $routeParams.bookId.
 
+  
   // Using this property to create the URL in line 9 of views/book.html
   $scope.currentBookIndex = parseInt($routeParams.bookId);
   
